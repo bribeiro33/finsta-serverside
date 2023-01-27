b50f8291f6c6483e5b3fba777dd19e46ab932cbf
Barbara Dreyer Rentz Ribeiro <53235549+bribeiro33@users.noreply.github.com>
2023-01-27 14:42:34 -0500

    mostly working index, ex login and cookies

--- insta485/views/index.py
+++ insta485/views/index.py
@@ -9,13 +9,13 @@ import flask
 import insta485
 import arrow
 
-# # @insta485.app.route('/uploads/<path:filename>')
-# # def file_url(filename):
-# #     """Return picture."""
-# #     if 'username' in flask.session:
-# #         return flask.send_from_directory(insta485.app.config['UPLOAD_FOLDER'],
-# #                                          filename, as_attachment=True)
-# #     flask.abort(403)
+@insta485.app.route('/uploads/<path:filename>')
+def file_url(filename):
+    """Return picture."""
+    #if 'username' in flask.session:
+    return flask.send_from_directory(insta485.app.config['UPLOAD_FOLDER'],
+                                         filename, as_attachment=True)
+    flask.abort(404)
 
 # # @insta485.app.route('/accounts/login', methods=['GET', 'POST'])
 # # def login():
@@ -32,51 +32,70 @@ def show_index():
     connection = insta485.model.get_db()
 
     
-    # Query database
+    # Query posts
     user = "awdeorio"
     cur = connection.execute(
-        "SELECT postid, filename AS img_url, owner, created "
+        "SELECT postid, filename, owner, created "
         "FROM posts "
         "WHERE owner = ? "
-
-        # "UNION "
-
-        # "SELECT username2 "
-        # "FROM following "
-        # "WHERE username1 = ?"
-
-        # "SELECT postid, filename AS img_url, owner, created "
-        # "FROM posts "
-        # "WHERE owner = username2 "
-    
+        "UNION "
+        "SELECT posts.postid, posts.filename, posts.owner, posts.created "
+        "FROM following JOIN posts "
+        "ON following.username2 = posts.owner "
+        "WHERE following.username1 == ? "
         "ORDER BY postid DESC",
-        (user, )
+        (user, user, )
     )
     posts = cur.fetchall()
     for post in posts:
-        # correct timestamp format
+        #post['img_url'] = "/./var/uploads/" + post['filename']
+        post['img_url'] = flask.url_for("file_url", filename=post['filename'])
+
+        # Correct timestamp format
         post['timestamp'] = arrow.get(post['created']).humanize()
 
-        # post's owner's icon url
+        # GET post's owner's icon url
         cur_owner = connection.execute(
             "SELECT filename "
             "FROM users "
             "WHERE username = ?",
             (post['owner'], )
         )
-        post['owner_img_url'] = cur_owner.fetchone()['filename']
+        post['owner_img_url'] = flask.url_for("file_url", filename=cur_owner.fetchone()['filename'])
 
+        # GET comments
         cur_comments = connection.execute(
-            "SELECT postid, owner, text, created "
+            "SELECT commentid, owner, postid, text "
             "FROM comments "
             "WHERE postid = ? "
-            "ORDER BY created ASC",
+            "ORDER BY commentid ASC",
             (post['postid'], )
         )
-        comments = cur_comments.fetchall()
-        for comment in comments:
-            post['comments.owner'] = comment['owner']
-            post['comments.text'] = comment['text']
+        post['comments'] = cur_comments.fetchall()
+
+        # GET likes
+        cur_likes = connection.execute(
+            "SELECT COUNT(owner) AS num_likes "
+            "FROM likes "
+            "WHERE postid = ?",
+            (post['postid'], )
+        )
+
+        post['likes'] = cur_likes.fetchone()['num_likes']
+
+        # GET if user likes the post
+        cur_user_likes = connection.execute(
+            "SELECT owner "
+            "FROM likes "
+            "WHERE owner=? AND postid=?",
+            [user, post['postid']]
+        )
+
+        # Will return an entry if user likes the post
+        if cur_user_likes.fetchone():
+            post['user_likes_it'] = True
+        else: 
+            post['user_likes_it'] = False
 
 
 
